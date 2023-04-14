d63bdea0a8db67d724a38b444846276f2efac847
Tomas Ruiz <tomas.ruiz@preisenergie.de>
2023-04-14 23:58:55 +0200

    Remove cache-related headers from monitor

--- monitor.sh
+++ monitor.sh
@@ -1,2 +1,2 @@
 watch --interval 1 \
-  "curl --silent -H 'Cache-Control: max-age=0' https://raw.githubusercontent.com/tomasruizt/git-csv-database/main/query-database.sql | duckdb"
+  "curl --silent https://raw.githubusercontent.com/tomasruizt/git-csv-database/main/query-database.sql | duckdb"
